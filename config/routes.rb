31eb1beac729a4c4b79f993cd334567bbf917daa
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 18:57:17 -0500

    Add chasqui location resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :locations
-  resources :chasquis
+  resources :chasquis do
+    resources :locations
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
