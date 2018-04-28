38a56acbb9d83cd4d23a092a349f13c2e478fd8c
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 18:30:06 -0500

    Add chasqui to location schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_28_231132) do
+ActiveRecord::Schema.define(version: 2018_04_28_232239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,11 @@ ActiveRecord::Schema.define(version: 2018_04_28_231132) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.bigint "chasqui_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["chasqui_id"], name: "index_locations_on_chasqui_id"
   end
 
+  add_foreign_key "locations", "chasquis"
 end
