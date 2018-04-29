aa5866abd14b3bfda62429c46fd13419b9fbaf38
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 23:45:58 -0500

    Create marks

---
+++ db/migrate/20180429044445_create_marks.rb
@@ -0,0 +1,10 @@
+class CreateMarks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :marks do |t|
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
