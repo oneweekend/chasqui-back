13437b65548fffc5464d3066cc9baf746cbe5eb0
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 20:30:01 -0500

    Create sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,7 @@
 class SessionsController < ApplicationController
-  before_action :set_session, only: [:show, :update, :destroy]
+  before_action :set_session, only: %i[show update destroy]
 
-  # GET /sessions
-  def index
-    @sessions = Session.all
-
-    render json: @sessions
-  end
-
-  # GET /sessions/1
-  def show
-    render json: @session
-  end
-
-  # POST /sessions
+  # POST /users/:user_email/sessions
   def create
     @session = Session.new(session_params)
 
@@ -24,28 +12,14 @@ class SessionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sessions/1
-  def update
-    if @session.update(session_params)
-      render json: @session
-    else
-      render json: @session.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /sessions/1
+  # DELETE /users/:user_email/session
   def destroy
     @session.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_session
-      @session = Session.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def session_params
-      params.require(:session).permit(:user_id)
-    end
+  def session_params
+    { user: User.find_by(email: params[:email]) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :users do
-    resource :session
     resources :locations
   end
+
+  resource :session, only: %i[create destroy]
 
   resources :chasquis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
