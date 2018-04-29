b962c70ff20c8669d6401f0881a46d12f4857218
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 19:48:00 -0500

    Fix user locations method

--- app/models/chasqui.rb
+++ app/models/chasqui.rb
@@ -1,6 +1,4 @@
 class Chasqui < User
-  has_many :locations
-
   def current_location
     locations.order(created_at: :desc).take
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_one :session
-  has_many :locations, foreign_key: 'user_id'
+  has_many :locations
 end
