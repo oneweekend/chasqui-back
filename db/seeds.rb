ce1c76682bb7aedb22d5ed5b3684e94c72db9b12
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-29 13:54:15 -0500

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-luis =      FactoryBot.create(:user, name: nil, email: nil )
-karina =    FactoryBot.create(:user, name: 'Karina Buhezo',
-                           email: 'karina@chasqui.com',
-                           phone: '987654321')
-katherine = Chasqui.create(name: 'Katherine Ortega',
-                           email: 'kath@chasqui.com',
-                           phone: '998877665')
-cinthia =   Chasqui.create(name: 'Cinthia Lazaro',
-                           email: 'cinthia@chasqui.com',
-                           phone: '912331238')
-fiorella =  Chasqui.create(name: 'Joissy Fiorella',
-                           email: 'fiore@chasqui.com',
-                           phone: '986754231')
