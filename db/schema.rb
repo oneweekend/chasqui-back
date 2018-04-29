967c5ffa115b768144c10eddd3f90a2ff545ee7c
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 19:24:22 -0500

    Migrate sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_28_232239) do
+ActiveRecord::Schema.define(version: 2018_04_29_002040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,19 @@ ActiveRecord::Schema.define(version: 2018_04_28_232239) do
     t.index ["chasqui_id"], name: "index_locations_on_chasqui_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "locations", "chasquis"
+  add_foreign_key "sessions", "users"
 end
