68dabf32f902241705e9a78006ab222d88484723
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-28 20:32:29 -0500

    Add unique index to sessions user_id

--- db/migrate/20180429013129_create_sessions.rb
+++ db/migrate/20180429013129_create_sessions.rb
@@ -1,7 +1,7 @@
 class CreateSessions < ActiveRecord::Migration[5.2]
   def change
     create_table :sessions do |t|
-      t.references :user, foreign_key: true
+      t.references :user, index: {:unique=>true}, foreign_key: true
 
       t.timestamps
     end
